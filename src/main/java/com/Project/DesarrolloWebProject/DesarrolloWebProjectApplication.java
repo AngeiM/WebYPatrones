7d499ca01b8bb98a9b17097dd42a5dd30fc09d47
Angelica Marín <marinange22@gmail.com>
2023-02-05 06:19:56 -0600

    Finalizacion de TLOU y RDR2

---
+++ src/main/java/com/Project/DesarrolloWebProject/DesarrolloWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Project.DesarrolloWebProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarrolloWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesarrolloWebProjectApplication.class, args);
+	}
+
+}
